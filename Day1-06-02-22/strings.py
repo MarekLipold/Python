a966d018f6ade0a6f7ce69632eb42dd019795159
Marek Lipold <lipoldmarek20@gmail.com>
2022-06-03 10:19:38 -0400

    Day1-06-02-22

--- Day1-06-02-22/strings.py
+++ Day1-06-02-22/strings.py
@@ -1,2 +1,17 @@
 # Strings
 # Learning python strings
+"Marek"
+'Lipold'
+"I'm a new developer"
+
+print("Hello world")
+print ("Hello" + "world") # Helloworld
+
+print ("3" + "5") # 35
+print (3 + 5) # 8
+print (3.14 * 2) # 6.28
+
+True # 1
+False # 0
+
+print (True + True)
