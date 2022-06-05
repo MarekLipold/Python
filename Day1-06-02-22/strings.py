2d19c81d9783556a88c41b67f800f5ddcf4e951c
Marek Lipold <lipoldmarek20@gmail.com>
2022-06-05 15:33:42 -0400

    .

--- Day1-06-02-22/strings.py
+++ Day1-06-02-22/strings.py
@@ -1,17 +1,50 @@
-# Strings
-# Learning python strings
-"Marek"
-'Lipold'
-"I'm a new developer"
+# # Strings
+# # Learning python strings
+# "Marek"
+# 'Lipold'
+# "I'm a new developer"
 
-print("Hello world")
-print ("Hello" + "world") # Helloworld
+# print("Hello world")
+# print ("Hello" + "world") # Helloworld
 
-print ("3" + "5") # 35
-print (3 + 5) # 8
-print (3.14 * 2) # 6.28
+# print ("3" + "5") # 35
+# print (3 + 5) # 8
+# print (3.14 * 2) # 6.28
 
-True # 1
-False # 0
+# True # 1
+# False # 0
 
-print (True + True)
+# print (True + True)
+
+# # print (8.0/3.14)
+# first_name = "Marek"
+# last_name = "Lipold"
+# # print "Marek Lipold"
+# print first_name, last_name
+# print first_name + last_name
+
+# found_coins = 20
+# magic_coins = 10
+# stolen_coins = 3
+
+# result = found_coins + magic_coins * 365 - stolen_coins * 52  #3514
+# print(result)
+
+# first_name = "Kim"
+# last_name = "Kardashian"
+
+# Hello Matt Fisher, How are you doing today?
+# sentence = "Hello {} {}, how are you doing today".format(first_name, last_name)
+# print(sentence)
+# sentence2 = "Hello {} {}, how are you doing today".format(first_name, last_name)
+# sentence3 = "Hello {1} {0}, how are you doing today".format(first_name, last_name)
+# print (sentence3)
+
+# sentence3 = "Hello {first} {last} {last} {last}, how are you doing today".format(first=first_name, last=last_name)
+# print(sentence3)
+
+first_name = "Kim"
+last_name = "Kardashian"
+
+sentence = f'Hello {first_name} {last_name} how are you doing today?'
+print(sentence)
