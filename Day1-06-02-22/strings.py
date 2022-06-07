29104d9785c87ed7ab34eb5c51852d3d5e0d4333
Marek Lipold <lipoldmarek20@gmail.com>
2022-06-07 18:02:37 -0400

    .

--- Day1-06-02-22/strings.py
+++ Day1-06-02-22/strings.py
@@ -43,8 +43,90 @@
 # sentence3 = "Hello {first} {last} {last} {last}, how are you doing today".format(first=first_name, last=last_name)
 # print(sentence3)
 
-first_name = "Kim"
-last_name = "Kardashian"
+# first_name = "Kim"
+# last_name = "Kardashian"
 
-sentence = f'Hello {first_name} {last_name} how are you doing today?'
-print(sentence)
+# sentence = f'Hello {first_name} {last_name} how are you doing today?'
+# print(sentence)
+
+# print (type(first_name))
+
+# print( isinstance(99, int) )
+
+# name = input("what is your name? ")
+# print(f'your name is {name}')
+
+# print("hello world")
+# print("digital crafts")
+
+# age = 25
+# name = "Emily"
+# if age == 25:
+#     print(1)
+#     print(2)
+#     print(3)
+
+# if name == "Emily":
+#     print("Emily is here")
+
+# age = 19
+# if (age >=21):
+#     print ("you can drink")
+# elif age < 18:
+#         print("what are you trying to do?")
+# else:
+#     print("you are too young to be here")
+
+# print("outside of my if statement")
+
+
+
+# letter = input("Please enter a letter: ")
+# lower_letter = letter.lower()
+
+# if (lower_letter == "a" or lower_letter == "e" or lower_letter == "i" or lower_letter == "o" or lower_letter == "u"):
+#     print("Vowel")
+# elif lower_letter == "y":
+#     print("sometimes a vowel")
+# else:
+#     print("Not Vowel")
+
+#caste
+# age = int(input("what is your age? "))
+
+# print(type(age))
+
+# new_age = age + 5
+
+# print (new_age)
+
+# print("christine")
+
+# counter = 0 # 0-10
+
+# while counter <= 10:
+#     print("christine")
+#     counter = counter + 1 #counter = 0 + 1
+
+# while True:
+#     answer = input("Say when: ")
+
+#     if answer.lower() == "when":
+#         break
+# print("outside of while loop")
+
+number = int(input("Type a number "))
+counter = 0
+while True:
+    (number != number % 2 == 0) & (counter == 0)
+    print("enter new number")
+    if number == (number % 2) == 0:
+        break
+print("Even")
+if (number % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+
